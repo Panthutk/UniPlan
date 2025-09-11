58b02c879d374cc0390d9ee00623cc68fc557878
Pannawit Mahacharoensiri <pannawit.m@ku.th>
2025-09-11 19:47:21 +0700

    Finish Objective and Background parts in About Us page

--- frontend/src/about.jsx
+++ frontend/src/about.jsx
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router-dom";
+import Picture from "./assets/AdobeStock_433725201.jpeg";
+
 
 export default function About() {
   const navigate = useNavigate();
@@ -42,12 +44,12 @@ export default function About() {
             WHAT WE DO
           </h1>
         </header>
-        <p className="text-white my-8 text-3xl w-full px-[100px] leading-normal">
+        <p className="text-white my-9 text-2xl w-full px-[100px] leading-normal">
           We developed an "Integrated platform" that combines <br/>
           task management with class timetables,<br />
           designed to help users stay organized with ease.
         </p>
-        <ul className="text-[#578f67] list-disc list-inside text-white mb-[140px] text-lg w-full px-[100px]">
+        <ul className=" list-disc list-inside mb-[140px] text-lg w-full px-[100px] text-[#578f67] ">
           <li className="leading-loose">Users will have the ability to add, remove, and modify classes directly within the timetable.</li>
           <li className="leading-loose">Homework assignments can be created directly from a class block on the timetable.</li>
           <li className="leading-loose">Users will receive email notifications via Gmail regarding upcoming task due dates.</li>
@@ -59,23 +61,48 @@ export default function About() {
       <div className="w-full px-3 sm:px-8 min-h-[700px]" style={{ backgroundColor: "#212121" }}>
           {/*Header*/}
           <header className="bg-transparent p-5 text-center">
-            <h1 className="text-[#3e6d4a] mt-8 text-6xl font-bold tracking-wide">
+            <h1 className="text-[#3e6d4a] mt-9 text-6xl font-bold tracking-wide">
               WHY WE DO
             </h1>
           </header>
 
           {/*Seperate each reason to be 3 reasons*/}
-            <div className="flex justify-center space-x-20 mt-10 min-h-[400px]">
-              <ReasonCard info_text="Reason 1: This is a longer text that should wrap nicely inside the card to multiple lines if needed."/>
-              <ReasonCard info_text="Reason 2"/>
-              <ReasonCard info_text="Reason 3"/>
+            <div className="flex flex-col md:flex-row justify-center md:items-stretch items-center gap-20 mt-10 min-h-[400px]">
+              <ReasonCard info_text="Many students struggle to manage multiple timetables and assignment deadlines, which can lead to stress, disorganization, and overlooked tasks."/>
+              <ReasonCard info_text="Losing track of assignments and due dates negatively impacts students’ performance and overall academic success."/>
+              <ReasonCard info_text="There are currently no applications that integrate Google Classroom with timetables and task management, which is why we decided to develop one ourselves."/>
             </div>
       </div>
 
-      {/*Third text box : Who are we*/}
-      <div className="mt-24 bg-transparent">
+      
+    {/* Third text box: Who are we */}
+    <div className="mt-9 w-full px-3 sm:px-8 min-h-[700px]">
+      <header className="bg-transparent p-5 flex justify-end">
+        <h1 className="text-[#3e6d4a] text-6xl font-bold tracking-wide">
+          WHO WE ARE
+        </h1>
+      </header>
 
+      {/* Flex container for left-right */}
+      <div className="flex flex-col md:flex-row mt-8 gap-6">
+        {/* Left: picture */}
+        <div className="md:w-2/3">
+          <img
+            src= {Picture}
+            alt="Team member"
+            className="w-full h-auto rounded-lg object-cover"
+          />
+        </div>
+
+        {/* Right: content */}
+        <div className="md:w-1/3 flex flex-col">
+          <h className="px-6 text-3xl text-[#578f67] ">Name</h>
+          {/* <p className="px-10 mt-4 mb-3 text-xl bg-transparent">{name}</p> */}
+          <h className="px-6 text-3xl text-[#578f67] ">Email</h>
+          {/* <p className="px-10 mt-4 mb-3 text-xl bg-transparent">{email}</p> */}
+        </div>
       </div>
+    </div>
       
     </div>
   );
@@ -83,8 +110,10 @@ export default function About() {
 
 function ReasonCard({info_text}) {
   return (
-    <div className="p-6 rounded-xl shadow-md transform transition-transform duration-300 hover:scale-125 w-72 break-words" style={{ backgroundColor: "#40704e" }}>
-      <h2 className="text-xl font-bold mb-2">{info_text}</h2>
+    <div className="p-6 flex rounded-2xl shadow-md transform items-center text-center justify-center transition-transform 
+    duration-300 bg-[#40704e] hover:scale-125 hover:bg-[#24452d] w-72 break-words" >
+      <h2 className="text-xl mb-2 leading-loose">{info_text}</h2>
     </div>
   )
 }
+
